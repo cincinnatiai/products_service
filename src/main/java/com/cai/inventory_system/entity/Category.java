9c689603d994c2a0ca2c5cc5c95b4dfecccfe27c
albertoochoagalvez <betangas8a@gmail.com>
2025-06-26 11:01:43 -0400

    definition of the entities

---
+++ src/main/java/com/cai/inventory_system/entity/Category.java
@@ -0,0 +1,25 @@
+package com.cai.inventory_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String created_at;
+    private String updated_at;
+
+}
+
