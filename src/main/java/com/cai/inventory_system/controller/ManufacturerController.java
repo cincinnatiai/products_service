53aafecf6ff7c42c0db43bb804c9f86544129329
JuanPaHdezG <117241354+JuanPaHdezG@users.noreply.github.com>
2025-07-11 17:54:45 -0400

    Manufacturer Controller Test (#14)
    * Manufacturer Controller Test
    
    * Pagination methosd and testing
    
    ---------
    
    Co-authored-by: Cincinnati AI Dev 2 <cincinnatiaidev2@MacBook-Air.local>
    Co-authored-by: Cincinnati AI Dev 2 <cincinnatiaidev2@192.168.1.9>

--- src/main/java/com/cai/inventory_system/controller/ManufacturerController.java
+++ src/main/java/com/cai/inventory_system/controller/ManufacturerController.java
@@ -3,6 +3,10 @@ package com.cai.inventory_system.controller;
 import com.cai.inventory_system.dto.ManufacturerDTO;
 import com.cai.inventory_system.service.ManufacturerService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -46,4 +50,20 @@ public class ManufacturerController {
         manufacturerService.deleteManufacturer(manufacturerId);
         return ResponseEntity.ok("Manufacturer deleted successfully!");
     }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<ManufacturerDTO>> getManufacturersByPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction)
+    {
+        Sort.Direction dir = direction.equalsIgnoreCase("desc") ?
+                Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(dir, sortBy));
+        Page<ManufacturerDTO> manufacturers = manufacturerService.getManufacturersByPage(pageable);
+        return ResponseEntity.ok(manufacturers);
+    }
+
 }
--- src/main/java/com/cai/inventory_system/service/ManufacturerService.java
+++ src/main/java/com/cai/inventory_system/service/ManufacturerService.java
@@ -1,6 +1,8 @@
 package com.cai.inventory_system.service;
 
 import com.cai.inventory_system.dto.ManufacturerDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ public interface ManufacturerService {
     ManufacturerDTO getManufacturerById(String id);
     void deleteManufacturer(String id);
     ManufacturerDTO updateManufacturer(String id, ManufacturerDTO manufacturerDTO);
+    Page<ManufacturerDTO> getManufacturersByPage(Pageable pageable);
 
 
 
