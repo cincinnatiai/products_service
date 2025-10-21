ea023af6aa12bc47d6aa43b5bf7c36f5262fe4cf
nicholaspark09 <nicholaspark09@gmail.com>
2025-10-21 15:24:22 -0400

    Add auth client

---
+++ src/main/java/com/cai/inventory_system/model/request/AuthenticationRequest.java
@@ -0,0 +1,26 @@
+package com.cai.inventory_system.model.request;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Request model for client authorization.
+ * Corresponds to Go's AuthenticationRequest struct.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthenticationRequest {
+    @SerializedName("method")
+    private String method;
+
+    @SerializedName("attempting_service_id")
+    private String attemptingServiceId;
+
+    @SerializedName("goal_service_key")
+    private String goalServiceKey;
+}
